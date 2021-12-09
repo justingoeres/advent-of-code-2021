7f11217dbe2d22d633b798634e7e3398195b2a23
Justin Goeres <jgoeres@liveareacx.com>
2021-12-09 11:28:20 -0500

    Day 9B Find largest smoke basins

--- src/main/java/org/jgoeres/adventofcode2021/Day09/Day09Service.java
+++ src/main/java/org/jgoeres/adventofcode2021/Day09/Day09Service.java
@@ -5,6 +5,7 @@ import org.jgoeres.adventofcode.common.DirectionNESW;
 import org.jgoeres.adventofcode.common.XYPoint;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.text.MessageFormat;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,6 +16,7 @@ public class Day09Service {
     private Map<XYPoint, Integer> heightMap = new HashMap<>();
     private Integer xSize;
     private Integer ySize;
+    private Set<XYPoint> lowPoints = new HashSet<>();
 
     public Day09Service(String pathToFile) {
         loadInputs(pathToFile);
@@ -37,6 +39,7 @@ public class Day09Service {
             for (int x = 0; x < xSize; x++) {
                 XYPoint xy = new XYPoint(x, y);
                 if (lowerThanNeighbors(xy, heightMap)) {
+                    lowPoints.add(xy);
                     totalRisk += riskLevel(heightMap.get(xy));
                 }
             }
@@ -49,11 +52,67 @@ public class Day09Service {
     public long doPartB() {
         System.out.println("=== DAY 9B ===");
 
-        long result = 0;
-        /** Put problem implementation here **/
+        /** What do you get if you multiply together the sizes of the three largest basins? **/
+
+        // Iterate through all the lowest points. For each one, find all the connected
+        // xy points that have height of < 9. For each one, add one to the size counter for the basin
+
+        // We should be able to solve this recursively.
+        // 1. Init the basinSize counter to 0.
+        // 2. Start at point xy (the low point)
+        // 3. Remove this point from the heightMap (so we don't check it twice)
+        // 4. Increment the basinSize counter
+        // 5. For each existing neighbor from HeightMap whose height is < 9...
+        // 6. repeat from 3.
+        // 7. when we finish, add the size of this basin to the TreeSet of all basins
+        List<Integer> basinSizes = new ArrayList<>();
+        // For all the lowPoints
+        for (XYPoint lowPoint : lowPoints) {
+            // 1. Init the basinSize counter to 0.
+            Integer basinSize = 0;
+            // 2. Start at point xy (the low point)
+//            System.out.println(MessageFormat
+//                    .format("--- Starting from low point ({0}, {1})",
+//                            lowPoint.getX(), lowPoint.getY()));
+            basinSize = findBasinSize(lowPoint, basinSize);
+
+            basinSizes.add(basinSize);
+        }
+        Collections.sort(basinSizes);
+        Collections.reverse(basinSizes);
+        // Once we've calculated all the sizes, find the three largest and multiply them
+        long result = 1;
+        for (int i = 0; i < 3; i++) {
+            result *= basinSizes.get(i);
+        }
 
         System.out.println("Day 9B: Answer = " + result);
         return result;
+    }
+
+    private Integer findBasinSize(XYPoint xy, Integer currentSize) {
+        // 3. Remove this point from the heightMap (so we don't check it twice)
+        currentSize++;
+//        System.out.println(MessageFormat
+//                .format("Checking point ({0}, {1})\theight:\t{2}\tsize:\t{3}", xy.getX(), xy.getY(),
+//                        heightMap.get(xy), currentSize));
+        heightMap.remove(xy);
+        final Set<Direction8Way> neighborDirections =
+                Set.of(Direction8Way.UP, Direction8Way.RIGHT, Direction8Way.DOWN,
+                        Direction8Way.LEFT);
+
+        // 4. Increment the basinSize counter
+        for (Direction8Way direction : neighborDirections) {
+            // 5. For each existing neighbor from HeightMap whose height is < 9...
+            XYPoint neighbor = xy.getRelativeLocation(direction);
+            if (heightMap.containsKey(neighbor)
+                    && heightMap.get(neighbor) < 9) {
+                currentSize = findBasinSize(neighbor, currentSize);
+            }
+        }
+        // If we've checked all the neighbors, return the size up the stack
+        return currentSize++;
+        // 6. repeat from 3.
     }
 
     private boolean lowerThanNeighbors(XYPoint xy, Map<XYPoint, Integer> heightMap) {
--- src/test/java/org/jgoeres/adventofcode2021/Day09Test.java
+++ src/test/java/org/jgoeres/adventofcode2021/Day09Test.java
@@ -19,12 +19,13 @@ public class Day09Test {
 
     @Test
     @Order(1)   // Run before Puzzle Part B
-    public void Day09A() {
+    public void Day09() {
         if (day09Service == null) {
             day09Service = new Day09Service(PUZZLE_INPUT, PUZZLE_DEBUG);
         }
 
-        final long EXPECTED = 550;
+        // Part A
+        final long PARTA_EXPECTED = 550;
         long result = 0;
         try {
             result = day09Service.doPartA();
@@ -32,56 +33,38 @@ public class Day09Test {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        assertEquals(EXPECTED, result);
-    }
+        assertEquals(PARTA_EXPECTED, result);
 
-    @Test
-    @Order(2)   // Run after Puzzle Part A
-    public void Day09B() {
-        if (day09Service == null) {
-            day09Service = new Day09Service(PUZZLE_INPUT, PUZZLE_DEBUG);
-        }
-
-        final long EXPECTED = 0;
-        long result = 0;
+        // Part B
+        final long PARTB_EXPECTED = 1100682;
         try {
             result = day09Service.doPartB();
             ToClipboard.set(result);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        assertEquals(EXPECTED, result);
+        assertEquals(PARTB_EXPECTED, result);
     }
 
     @Test
     @Order(3)   // Run before Example Part B
-    @Disabled
-    public void Day09AExample1() {
+    public void Day09Example1() {
         example1Service = new Day09Service(EXAMPLE1_INPUT, EXAMPLE_DEBUG);
-        final long EXPECTED = 0;
+        final long PARTA_EXPECTED = 15;
         long result = 0;
         try {
             result = example1Service.doPartA();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        assertEquals(EXPECTED, result);
-    }
+        assertEquals(PARTA_EXPECTED, result);
 
-    @Test
-    @Order(4)   // Run after Example Part A
-    @Disabled
-    public void Day09BExample1() {
-        // Instantiate the service if Part A was skipped
-        if (example1Service == null) example1Service = new Day09Service(EXAMPLE1_INPUT, EXAMPLE_DEBUG);
-
-        final long EXPECTED = 0;
-        long result = 0;
+        final long PARTB_EXPECTED = 1134;
         try {
             result = example1Service.doPartB();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        assertEquals(EXPECTED, result);
+        assertEquals(PARTB_EXPECTED, result);
     }
 }
