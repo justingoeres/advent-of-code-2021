c03ef460d22270d7176dd1bf08ba808483751ba4
Justin Goeres <justin.goeres@gmail.com>
2021-12-19 11:26:21 -0500

    Day 19A Rotate & match one scanner to the reference scanner

--- src/main/java/org/jgoeres/adventofcode2021/Day19/Day19Service.java
+++ src/main/java/org/jgoeres/adventofcode2021/Day19/Day19Service.java
@@ -1,10 +1,12 @@
 package org.jgoeres.adventofcode2021.Day19;
 
+import static org.jgoeres.adventofcode.common.XYZPoint.Axis.*;
+import static org.jgoeres.adventofcode.common.XYZPoint.ORIGIN_XYZ;
+import org.jgoeres.adventofcode.common.RotationStep;
 import org.jgoeres.adventofcode.common.XYZPoint;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,6 +14,8 @@ public class Day19Service {
     public boolean DEBUG = false;
 
     private final Map<Integer, Scanner> scanners = new HashMap<>();
+    private final Set<XYZPoint> knownUniverse = new HashSet<>();
+
 
     public Day19Service(String pathToFile) {
         loadInputs(pathToFile);
@@ -26,10 +30,113 @@ public class Day19Service {
         System.out.println("=== DAY 19A ===");
 
         long result = 0;
-        /** Put problem implementation here **/
+        /**
+         * The scanners and beacons map a single contiguous 3d region.
+         * This region can be reconstructed by finding pairs of scanners
+         * that have overlapping detection regions such that there are
+         * at least 12 beacons that both scanners detect within the
+         * overlap. By establishing 12 common beacons, you can precisely
+         * determine where the scanners are relative to each other,
+         * allowing you to reconstruct the beacon map one scanner at a
+         * time.
+         *
+         * Assemble the full map of beacons. How many beacons are there?
+         **/
+
+        // Add all of scanner 0 to the known universe
+        scanners.get(0).getBeacons().stream().forEach(xyz -> knownUniverse.add(xyz));
+
+        // If we take Scanner 0 as our reference for position & orientation
+        // Then Take point 0 of scanner 0.
+        // Calculate all the RELATIVE beacon coordinates TO THAT POINT.
+        // That gives us a search space anchored on a beacon that MAY exist
+        // on other scanners
+
+        // referenceBeacons are the set of beacons for the CURRENT scanner,
+        // translated so that ONE of the beacons is at 0,0,0
+        final Scanner referenceScanner = scanners.get(0);
+        final XYZPoint referenceBeacon = referenceScanner.getBeacons().get(0);
+        final List<XYZPoint> referenceBeacons =
+                referenceScanner.getBeaconsToReference(referenceBeacon);
+
+        // From there, take scanner 1.
+        // Take point 0 on scanner 1.
+        Scanner candidateScanner = scanners.get(1);
+        for (XYZPoint candidateReference : candidateScanner.getBeacons()) {
+            System.out.println(
+                    "Checking candidate reference point:\t" + candidateReference.toString());
+            // Calculate the RELATIVE beacon coordinates TO THAT POINT.
+            final List<XYZPoint> candidateBeacons =
+                    candidateScanner.getBeaconsToReference(candidateReference);
+            final Boolean foundMatchingRotation =
+                    checkAllRotations(referenceBeacons, candidateBeacons);
+            if (foundMatchingRotation) {
+                System.out.println("Found a match!");
+                // If we found a matching rotation, next we need to
+                // 1. translate all the beacons into "universe" coordinates
+                //      by moving them relative to the referenceBeacon
+                // 2. Add them to the known universe
+//                candidateBeacons.stream().forEach(xyz -> xyz.translate(referenceBeacon));
+//                candidateBeacons.stream().forEach(xyz -> knownUniverse.add(xyz));
+                candidateBeacons.stream().forEach(xyz -> {
+                    xyz.translate(referenceBeacon);
+                    knownUniverse.add(xyz);
+                });
+                break;
+            }
+        }
+        // So try again, but with the scanner-1-relative set ROTATED.
+
+        //
+        // If we get through all 24 rotations without a match
+        // start over with the NEXT point in scanner 1 (point 1).
+        //
+        // If we get all the way through all the scanner 1 points without a match,
+        // Repeat all of the above with scanner 2.
+
+        // If we DO find a match, then convert all that scanner's beacon
+        // coordinates to relative-to-scanner-0 and
+        // add them to our "known universe" Set(?)
+        // By doing this, we build up a "map of the universe" gradually
+        // And can keep matching scanners to it until we're done
+
 
         System.out.println("Day 19A: Answer = " + result);
         return result;
+    }
+
+    private Boolean checkAllRotations(final List<XYZPoint> referenceBeacons,
+                                      final List<XYZPoint> candidateBeacons) {
+        for (final RotationStep rotation : allRotations) {
+            // Rotate the candidateBeacons into the orientation we're testing
+            System.out.println(
+                    "Rotating around " + rotation.getAxis() + " " + rotation.getTimes() +
+                            " time(s)");
+            for (final XYZPoint beacon : candidateBeacons) {
+                beacon.rotate(rotation.getAxis(), rotation.getTimes());
+            }
+            // Now check all the candidate (rotated) candidate beacons against our reference
+            // If any match a coordinate in the relative-Scanner-0 list, increment
+            // a counter.
+            Integer matchCounter = 0;
+            for (final XYZPoint beacon : candidateBeacons) {
+                if (referenceBeacons.contains(beacon)) {
+                    matchCounter++;
+                    if (!beacon.equals(ORIGIN_XYZ)) {   // Of course the origin matches
+                        System.out.println(
+                                "Matched beacon at (" + beacon + ")\tMatch count:\t" +
+                                        matchCounter);
+                    }
+                }
+                // Can cut this off early if we need to
+                if (matchCounter >= 12) {
+                    System.out.println("We found at least 12 matches!!!");
+                    return true; // we found a match!
+                }
+            }
+        }
+        // If the counter doesn't reach 12, we do not have a match.
+        return false;
     }
 
     public long doPartB() {
@@ -67,8 +174,37 @@ public class Day19Service {
                     scanner.add(beacon);
                 }
             }
+            // Stick scanner 0 at 0,0,0
+            scanners.get(0).setPosition(new XYZPoint(0, 0, 0));
         } catch (Exception e) {
             System.out.println("Exception occurred: " + e.getMessage());
         }
     }
+
+    private static final List<RotationStep> allRotations = List.of(
+            new RotationStep(NONE), // First rotation is "just do the original state"
+            new RotationStep(X_AXIS),
+            new RotationStep(X_AXIS),
+            new RotationStep(X_AXIS),
+            new RotationStep(Z_AXIS, 2),
+            new RotationStep(X_AXIS),
+            new RotationStep(X_AXIS),
+            new RotationStep(X_AXIS),
+            new RotationStep(Z_AXIS, 1),
+            new RotationStep(X_AXIS),
+            new RotationStep(X_AXIS),
+            new RotationStep(X_AXIS),
+            new RotationStep(Z_AXIS, 2),
+            new RotationStep(X_AXIS),
+            new RotationStep(X_AXIS),
+            new RotationStep(X_AXIS),
+            new RotationStep(Y_AXIS, 1),
+            new RotationStep(X_AXIS),
+            new RotationStep(X_AXIS),
+            new RotationStep(X_AXIS),
+            new RotationStep(Z_AXIS, 2),
+            new RotationStep(X_AXIS),
+            new RotationStep(X_AXIS),
+            new RotationStep(X_AXIS)
+    );
 }
--- src/main/java/org/jgoeres/adventofcode2021/Day19/Scanner.java
+++ src/main/java/org/jgoeres/adventofcode2021/Day19/Scanner.java
@@ -1,11 +1,13 @@
 package org.jgoeres.adventofcode2021.Day19;
 
 import org.jgoeres.adventofcode.common.XYZPoint;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class Scanner {
-    private final Set<XYZPoint> beacons = new HashSet<>();
+    private final List<XYZPoint> beacons = new ArrayList<>();
+    private XYZPoint position;
 
     public Boolean add(XYZPoint xyz) {
         return beacons.add(xyz);
@@ -15,7 +17,25 @@ public class Scanner {
         return beacons.contains(xyz);
     }
 
-    public Set<XYZPoint> getBeacons() {
+    public List<XYZPoint> getBeaconsToReference(final XYZPoint reference) {
+        // Return a list of XYZPoints (our beacons) translated so that they're relative to the 'reference' point
+        final List<XYZPoint> beaconsToReference = new ArrayList<>();
+        beacons.stream().forEach(xyz -> beaconsToReference.add(new XYZPoint(
+                xyz.getX() - reference.getX(),
+                xyz.getY() - reference.getY(),
+                xyz.getZ() - reference.getZ())));
+        return beaconsToReference;
+    }
+
+    public XYZPoint getPosition() {
+        return position;
+    }
+
+    public void setPosition(XYZPoint position) {
+        this.position = position;
+    }
+
+    public List<XYZPoint> getBeacons() {
         return beacons;
     }
 }
